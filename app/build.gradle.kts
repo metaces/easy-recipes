2dc3fc27b41f5be0baac7f148d4684d855782064
C1321103 Cesar Felipe Reis Alberto <c1321103@interno.bb.com.br>
2024-07-26 00:02:35 -0300

    Criado tela de Splash screen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,17 +20,17 @@ android {
         versionCode = 1
         versionName = "1.0"
 
-        val localProperties = project.rootProject.file("local.properties")
-        val properties = Properties()
-        properties.load(localProperties.inputStream())
-
-        val apiKey = properties.getProperty("API_KEY")
-
-        buildConfigField(
-            type = "String",
-            name = "API_KEY",
-            value = apiKey
-        )
+//        val localProperties = project.rootProject.file("local.properties")
+//        val properties = Properties()
+//        properties.load(localProperties.inputStream())
+//
+//        val apiKey = properties.getProperty("API_KEY")
+//
+//        buildConfigField(
+//            type = "String",
+//            name = "API_KEY",
+//            value = apiKey
+//        )
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
--- app/src/main/java/com/devspace/myapplication/MainActivity.kt
+++ app/src/main/java/com/devspace/myapplication/MainActivity.kt
@@ -3,14 +3,43 @@ package com.devspace.myapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.devspace.myapplication.ui.theme.EasyRecipesTheme
+import kotlinx.coroutines.delay
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,7 +51,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    SplashScreen {
+
+                    }
                 }
             }
         }
@@ -30,17 +61,147 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun SplashScreen(onTimeout: () -> Unit) {
+    var showSplash by remember { mutableStateOf(true) }
+
+    if (showSplash) {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(MaterialTheme.colorScheme.primary), // Customize background
+            contentAlignment = Alignment.Center
+        ) {
+            // Add your splash screen content here
+            Image(
+                painter = painterResource(id = R.drawable.onboarding), // Replace with your image
+                contentDescription = "Splash Image",
+                modifier = Modifier.fillMaxSize(),
+                contentScale = ContentScale.Crop
+            )
+            Column(
+                modifier = Modifier.align(Alignment.BottomCenter).padding(16.dp)
+            ) {
+                Text(
+                    text = "Let`s",
+                    color = Color.White,
+                    fontSize = 44.sp,
+                    fontWeight = FontWeight.Bold,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier.fillMaxWidth()
+                )
+                Text(
+                    text = "Cooking",
+                    color = Color.White,
+                    fontSize = 44.sp,
+                    fontWeight = FontWeight.Bold,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier.fillMaxWidth()
+                )
+                Spacer(modifier = Modifier.height(8.dp)) // Add some space
+                Text(
+                    text = "Find best recipes for cooking",
+                    color = Color.White,
+                    fontSize = 14.sp,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier.fillMaxWidth()
+                )
+                Spacer(modifier = Modifier.height(8.dp)) // Add some space
+                Button(
+                    modifier = Modifier
+                        .align(Alignment.CenterHorizontally),
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = Color.Red,
+                        contentColor = Color.White
+                    ),
+                    shape = RoundedCornerShape(10.dp),
+                    onClick = {
+                        showSplash = false
+                    }) {
+                    Text("Start cooking")
+                }
+            }
+        }
+    } else {
+        onTimeout()
+    }
+}
+
+@Composable
+fun MyApp() {
+    var showSplashScreen by remember { mutableStateOf(true) }
+
+    if (showSplashScreen) {
+        SplashScreen {
+            showSplashScreen = false
+        }
+    } else {
+        // Your main app content
+        MainScreenContent()
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    EasyRecipesTheme {
-        Greeting("Android")
+fun MyAppPreview() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colorScheme.primary), // Customize background
+        contentAlignment = Alignment.Center
+    ) {
+        // Add your splash screen content here
+        Image(
+            painter = painterResource(id = R.drawable.onboarding), // Replace with your image
+            contentDescription = "Splash Image",
+            modifier = Modifier.fillMaxSize(),
+            contentScale = ContentScale.Crop
+        )
+        Column(
+            modifier = Modifier.align(Alignment.BottomCenter).padding(16.dp)
+        ) {
+            Text(
+                text = "Let`s",
+                color = Color.White,
+                fontSize = 44.sp,
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Center,
+                modifier = Modifier.fillMaxWidth()
+            )
+            Text(
+                text = "Cooking",
+                color = Color.White,
+                fontSize = 44.sp,
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Center,
+                modifier = Modifier.fillMaxWidth()
+            )
+            Spacer(modifier = Modifier.height(8.dp)) // Add some space
+            Text(
+                text = "Find best recipes for cooking",
+                color = Color.White,
+                fontSize = 14.sp,
+                textAlign = TextAlign.Center,
+                modifier = Modifier.fillMaxWidth()
+            )
+            Spacer(modifier = Modifier.height(8.dp)) // Add some space
+            Button(
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color.Red,
+                    contentColor = Color.White
+                ),
+                shape = RoundedCornerShape(10.dp),
+                onClick = { /*TODO*/ }) {
+                Text("Start cooking")
+            }
+        }
     }
 }
+
+@Composable
+fun MainScreenContent() {
+    Text(text = "Hello")
+}
+
+
